0c5c2ef71c27e0f95dc2d0a52acb2875463a0a7f
wilhelmandreibubeneck <andrei.bubeneck@yahoo.com>
2019-01-10 23:47:13 +0100

    added license to footer

--- footer.php
+++ footer.php
@@ -1,10 +1,11 @@
 <div id="footer">
-		Copyright &copy; <?php
+		&copy; <?php
 		if(date('Y') == 2014){
 			echo 2014;
 		}
 		else{
 			echo '2014'.'-'.date('Y');
 		}
-		?> <a href="mailto:wilhelm.andrei.bubeneck@ivao.aero">Wilhelm Andrei Bubeneck</a> (<a target='blank' href='https://ivao.aero/members/person/details.asp?id=335177'>335177</a>)
+		?> <a href="mailto:wilhelm.andrei.bubeneck@ivao.aero">Wilhelm Andrei Bubeneck</a>. Licensed under
+        <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>
 </div>
