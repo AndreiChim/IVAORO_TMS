20a77c0013eb8e3351ee7295d92a05517e28440c
wilhelmandreibubeneck <andrei.bubeneck@yahoo.com>
2019-08-21 01:53:56 +0300

    added trainer table on home page

--- home.php
+++ home.php
@@ -10,13 +10,140 @@
 		<h4>
 			After completing your training, you will be able to review the training report submitted by the trainer.
 			This will enable you to always have a complete overview of your own training history. <br><br>
-			If you have any questions about trainings, exams or GCA, do not hesitate to contact our training department: <br><br>
-			RO-TC <a href='mailto:ro-tc@ivao.aero'>Wilhelm Andrei Bubeneck</a><br>
+			If you have any questions about trainings, exams or GCA, do not hesitate to contact our training department:
+        </h4>
+        <br><br>
+        <table>
+            <tr>
+                <td class="tablekey">
+                    Name
+                </td>
+                <td class="tablekey">
+                    ATC Rating
+                </td>
+                <td class="tablekey">
+                    Pilot Rating
+                </td>
+                <td class="tablekey">
+                    Staff Position
+                </td>
+                <td class="tablekey">
+                    Roles
+                </td>
+                <td class="tablekey">
+                    Email
+                </td>
+            </tr>
+            <tr>
+                <td class="tablevalue">
+                    Andrei Bubeneck
+                </td>
+                <td class="tablevalue">
+                    ACC
+                </td>
+                <td class="tablevalue">
+                    ATP
+                </td>
+                <td class="tablevalue">
+                    RO-DIR/RO-TC
+                </td>
+                <td class="tablevalue">
+                    Examiner/Trainer
+                </td>
+                <td class="tablevalue">
+                    ro-tc@ivao.aero
+                </td>
+            </tr>
+            <tr>
+                <td class="tablevalue">
+                    Nicolae Podaru
+                </td>
+                <td class="tablevalue">
+                    ADC
+                </td>
+                <td class="tablevalue">
+                    SPP
+                </td>
+                <td class="tablevalue">
+                    RO-ADIR
+                </td>
+                <td class="tablevalue">
+                    Examiner/Trainer
+                </td>
+                <td class="tablevalue">
+                    ro-adir@ivao.aero
+                </td>
+            </tr>
+            <tr>
+                <td class="tablevalue">
+                    Udo Korbanka
+                </td>
+                <td class="tablevalue">
+                    ACC
+                </td>
+                <td class="tablevalue">
+                    PP
+                </td>
+                <td class="tablevalue">
+                    RO-TAC
+                </td>
+                <td class="tablevalue">
+                    Examiner/Trainer
+                </td>
+                <td class="tablevalue">
+                    ro-tac@ivao.aero
+                </td>
+            </tr>
+            <tr>
+                <td class="tablevalue">
+                    Lucian Cristea
+                </td>
+                <td class="tablevalue">
+                    APC
+                </td>
+                <td class="tablevalue">
+                    CP
+                </td>
+                <td class="tablevalue">
+                    RO-TA1
+                </td>
+                <td class="tablevalue">
+                    Examiner/Trainer
+                </td>
+                <td class="tablevalue">
+                    ro-ta1@ivao.aero
+                </td>
+            </tr>
+            <tr>
+                <td class="tablevalue">
+                    Ionut Trisca
+                </td>
+                <td class="tablevalue">
+                    ADC
+                </td>
+                <td class="tablevalue">
+                    PP
+                </td>
+                <td class="tablevalue">
+                    RO-T01
+                </td>
+                <td class="tablevalue">
+                    Trainer
+                </td>
+                <td class="tablevalue">
+                    ro-t01@ivao.aero
+                </td>
+            </tr>
+        </table>
+        <!--
+            RO-TC <a href='mailto:ro-tc@ivao.aero'>Wilhelm Andrei Bubeneck</a><br>
 			RO-TAC <a href='mailto:ro-tac@ivao.aero'>Udo Korbanka</a><br>
-            RO-TA1 <a href='mailto:ro-ta1@ivao.aero'>Lucian Cristea</a><br><br>
+            RO-TA1 <a href='mailto:ro-ta1@ivao.aero'>Lucian Cristea</a><br>
+            RO-T01 <a href='mailto:ro-t01@ivao.aero'>Ionut Trisca</a><br>
+        -->
+            <br>
 			For questions about this system, contact 
 			<a href='mailto:ro-tc@ivao.aero'>ro-tc@ivao.aero</a>.
-		</h4>
 		<h4>
 			Enjoy!
 		</h4>
