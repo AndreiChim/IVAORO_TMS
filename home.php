4ccd4f3b83ecfba4ef97dd0650f052420b11d9a6
wilhelmandreibubeneck <andrei.bubeneck@yahoo.com>
2019-09-20 13:08:18 +0200

    VIDs instead of names visible on homepage trainer table

--- home.php
+++ home.php
@@ -12,6 +12,9 @@
 			This will enable you to always have a complete overview of your own training history. <br><br>
 			If you have any questions about trainings, exams or GCA, do not hesitate to contact our training department:
         </h4>
+
+        <?php if(isset($_SESSION['login'])){ ?>
+
         <table id="mytrainings" class="admintrainings">
             <tr>
                 <td class="tablekey">
@@ -154,6 +157,154 @@
                 </td>
             </tr>
         </table>
+
+        <?php } else{ ?>
+
+        <table id="mytrainings" class="admintrainings">
+            <tr>
+                <td class="tablekey">
+                    VID
+                </td>
+                <td class="tablekey">
+                    ATC Rating
+                </td>
+                <td class="tablekey">
+                    Pilot Rating
+                </td>
+                <td class="tablekey">
+                    Staff Position
+                </td>
+                <td class="tablekey">
+                    Roles
+                </td>
+                <td class="tablekey">
+                    Email
+                </td>
+            </tr>
+            <tr>
+                <td class="tablevalue">
+                    508320
+                </td>
+                <td class="tablevalue">
+                    <img src="https://www.ivao.aero/data/images/ratings/atc/5.gif" alt="ADC"/>
+                </td>
+                <td class="tablevalue">
+                    <img src="https://www.ivao.aero/data/images/ratings/pilot/6.gif" alt="SPP"/>
+                </td>
+                <td class="tablevalue">
+                    RO-ADIR
+                </td>
+                <td class="tablevalue">
+                    Examiner/ Trainer
+                </td>
+                <td class="tablevalue">
+                    ro-adir@ivao.aero
+                </td>
+            </tr>
+            <tr>
+                <td class="tablevalue">
+                    335177
+                </td>
+                <td class="tablevalue">
+                    <img src="https://www.ivao.aero/data/images/ratings/atc/7.gif" alt="ACC"/>
+                </td>
+                <td class="tablevalue">
+                    <img src="https://www.ivao.aero/data/images/ratings/pilot/8.gif" alt="ATP"/>
+                </td>
+                <td class="tablevalue">
+                    RO-TC
+                </td>
+                <td class="tablevalue">
+                    Examiner/ Trainer
+                </td>
+                <td class="tablevalue">
+                    ro-tc@ivao.aero
+                </td>
+            </tr>
+            <tr>
+                <td class="tablevalue">
+                    187023
+                </td>
+                <td class="tablevalue">
+                    <img src="https://www.ivao.aero/data/images/ratings/atc/7.gif" alt="ACC"/>
+                </td>
+                <td class="tablevalue">
+                    <img src="https://www.ivao.aero/data/images/ratings/pilot/5.gif" alt="PP"/>
+                </td>
+                <td class="tablevalue">
+                    RO-TAC
+                </td>
+                <td class="tablevalue">
+                    Examiner/ Trainer
+                </td>
+                <td class="tablevalue">
+                    ro-tac@ivao.aero
+                </td>
+            </tr>
+            <tr>
+                <td class="tablevalue">
+                    361327
+                </td>
+                <td class="tablevalue">
+                    <img src="https://www.ivao.aero/data/images/ratings/atc/6.gif" alt="APC"/>
+                </td>
+                <td class="tablevalue">
+                    <img src="https://www.ivao.aero/data/images/ratings/pilot/7.gif" alt="CP"/>
+                </td>
+                <td class="tablevalue">
+                    RO-TA1
+                </td>
+                <td class="tablevalue">
+                    Examiner/ Trainer
+                </td>
+                <td class="tablevalue">
+                    ro-ta1@ivao.aero
+                </td>
+            </tr>
+            <tr>
+                <td class="tablevalue">
+                    576966
+                </td>
+                <td class="tablevalue">
+                    <img src="https://www.ivao.aero/data/images/ratings/atc/5.gif" alt="ADC"/>
+                </td>
+                <td class="tablevalue">
+                    <img src="https://www.ivao.aero/data/images/ratings/pilot/5.gif" alt="PP"/>
+                </td>
+                <td class="tablevalue">
+                    RO-T01
+                </td>
+                <td class="tablevalue">
+                    Trainer
+                </td>
+                <td class="tablevalue">
+                    ro-t01@ivao.aero
+                </td>
+            </tr>
+            <tr>
+                <td class="tablevalue">
+                    494745
+                </td>
+                <td class="tablevalue">
+                    <img src="https://www.ivao.aero/data/images/ratings/atc/5.gif" alt="ADC"/>
+                </td>
+                <td class="tablevalue">
+                    <img src="https://www.ivao.aero/data/images/ratings/pilot/5.gif" alt="PP"/>
+                </td>
+                <td class="tablevalue">
+                    RO-T02
+                </td>
+                <td class="tablevalue">
+                    Trainer
+                </td>
+                <td class="tablevalue">
+                    ro-t02@ivao.aero
+                </td>
+            </tr>
+        </table>
+
+        <?php } ?>
+
         <!--
             RO-TC <a href='mailto:ro-tc@ivao.aero'>Wilhelm Andrei Bubeneck</a><br>
 			RO-TAC <a href='mailto:ro-tac@ivao.aero'>Udo Korbanka</a><br>
